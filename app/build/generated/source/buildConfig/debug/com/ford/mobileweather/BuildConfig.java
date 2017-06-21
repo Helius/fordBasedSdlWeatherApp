379acfa17b564b9dc29d30eb51333c77fc50da84
Eugene Samoylov <e.samoylov@2Gis.ru>
2017-06-21 19:31:16 +0700

    init

---
+++ app/build/generated/source/buildConfig/debug/com/ford/mobileweather/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ford.mobileweather;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ford.mobileweather";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 26;
+  public static final String VERSION_NAME = "1.7.14 ";
+}
